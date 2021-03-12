b6519bbb75d8d074d57b828a5276021caaa9ca04
Jadyn <jadyn.wong@live.com>
2021-03-13 02:42:40 +0800

    update test-nop.ps1

--- test-nop.ps1
+++ test-nop.ps1
@@ -20,32 +20,29 @@ try {
     
     Start-Sleep -Milliseconds 10000
     
-    Write-Output "install"
+    Write-Output "Install"
     $content = Invoke-WebRequest "https://localhost:5001/Install" -ContentType 'application/x-www-form-urlencoded;charset=UTF-8' -Method POST -Body "AdminEmail=admin%40demo.com&AdminPassword=admin&ConfirmPassword=admin&Country=CN-zh-CN&InstallSampleData=true&DataProvider=1&CreateDatabaseIfNotExists=true&ServerName=localhost&DatabaseName=$Name&Username=sa&Password=Qwer%21234&ConnectionString=&Collation=&__RequestVerificationToken=CfDJ8ObQTlopsWVItdUlEBbDA6uYUwrkRY98tu1rbsmUsOOe-76_vpZdMo6L_sXoKve_Jq8COC7Pc-5UjnWj6MMJvGF2qGlp0yNVUbmt3OqLDGAmmX1ZhbKrC3tcTEaUTp8x3l3INLz-OEskj3NhxKRrlIY&InstallSampleData=false&CreateDatabaseIfNotExists=false&ConnectionStringRaw=false&IntegratedSecurity=false&UseCustomCollation=false"
     
     Write-Output "StatusCode: $($content.StatusCode)" 
-    Write-Output $content.Content
 
     if($content.Content.Contains("/Install/RestartApplication")){
+        Write-Output "RestartApplication"
+        Invoke-WebRequest "https://localhost:5001/Install/RestartApplication" -ContentType 'text/html;charset=UTF-8' -Method GET
         
-    }
-
-    Write-Output "restall"
-    Invoke-WebRequest "https://localhost:5001/Install/RestartApplication" -ContentType 'text/html;charset=UTF-8' -Method GET
+        Start-Process "powershell" "cd $outputFolder;dotnet Nop.Web.dll"
     
-    Start-Process "powershell" "cd $outputFolder;dotnet Nop.Web.dll"
-
-    Start-Sleep -Milliseconds 15000
-
-    Write-Output "jmeter run test"
-
-    for ($i = 1; $i -le 5; $i++) {
-        $sum += $i
-
-        $date = Get-Date -Format FileDateTime;
-        jmeter -n -t "Test Plan.jmx" -l $TestResultPath/$date.txt -e -o $TestResultPath/$date
-
-        Start-Sleep -Milliseconds 5000
+        Start-Sleep -Milliseconds 20000
+    
+        Write-Output "Jmeter run test"
+    
+        for ($i = 1; $i -le 5; $i++) {
+            $sum += $i
+    
+            $date = Get-Date -Format FileDateTime;
+            jmeter -n -t "Test Plan.jmx" -l $TestResultPath/$date.txt -e -o $TestResultPath/$date
+    
+            Start-Sleep -Milliseconds 5000
+        }
     }
 }
 catch {
